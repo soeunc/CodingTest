cf7aeda4be114f46b08dc5c6777233e9893b1d22
soeunc <fkckxk3910@gmail.com>
2024-01-24 19:37:07 +0900

    today algorithm

---
+++ src/Programmers/Lv0/Solution181918.java
@@ -0,0 +1,30 @@
+package Programmers.Lv0;
+
+
+import java.util.ArrayList;
+
+public class Solution181918 {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+
+        for (int i = 0; i < arr.length;) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                if (stk.get(stk.size() - 1) < arr[i]) {
+                    stk.add(arr[i]);
+                    i++;
+                } else {
+                    stk.remove(stk.size() - 1);
+                }
+            }
+        }
+        int[] result = new int[stk.size()];
+        for (int i = 0; i < stk.size(); i++) {
+            result[i] = stk.get(i);
+        }
+
+        return result;
+    }
+}
