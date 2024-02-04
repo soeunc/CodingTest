324faa78412057e866cf7a2cea38ede80b2280f9
soeunc <fkckxk3910@gmail.com>
2024-02-04 17:03:35 +0900

    commit: 1819110

---
+++ src/Programmers/Lv0/Solution181910.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0;
+
+public class Solution181910 {
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length() - n);
+    }
+}
