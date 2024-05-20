2a52e3c71eae60604bf4803a913c8ec9d179767b
soeunc <fkckxk3910@gmail.com>
2024-05-20 17:20:21 +0900

    add 1845

---
+++ src/Programmers/Lv0/Solution1845.java
@@ -0,0 +1,15 @@
+package Programmers.Lv0;
+
+import java.util.HashSet;
+
+public class Solution1845 {
+  public int solution(int[] nums) {
+    HashSet<Integer> result = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (!result.contains(nums[i]))
+        result.add(nums[i]);
+    }
+
+    return Math.min(result.size(), nums.length / 2);
+  }
+}
