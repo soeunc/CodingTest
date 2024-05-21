b70b09a304954d62abb1127fdf14d7b1f2781477
soeunc <fkckxk3910@gmail.com>
2024-05-21 19:52:47 +0900

    add 42576

---
+++ src/Programmers/Lv0/Solution42576.java
@@ -0,0 +1,39 @@
+package Programmers.Lv0;
+
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+public class Solution42576 {
+  public String solution(String[] participant, String[] completion) {
+    String answer = "";
+
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+
+    // 참가자 이름(키), 등장 횟수(값) 추가
+    for (int i = 0; i < participant.length; i++) {
+      if (map.containsKey(participant[i])) {
+        map.put(participant[i], map.get(participant[i]) + 1);
+      } else {
+        map.put(participant[i], 1);
+      }
+    }
+
+    // 완주한 사람(키), 등장 횟수(값) 제거
+    for (int i = 0; i < completion.length; i++) {
+      if (map.containsKey(completion[i])) {
+        if (map.get(completion[i]) == 1) {
+          map.remove(completion[i]);
+        } else {
+          map.replace(completion[i], map.get(completion[i]) - 1);
+        }
+      }
+    }
+
+    Set<String> set = map.keySet();
+    for (String key : set) {
+      answer = key;
+    }
+
+    return answer;
+  }
+}
