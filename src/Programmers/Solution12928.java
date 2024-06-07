47eaf71be54720dce6fb57ef1c1459a648ebc8b7
soeunc <fkckxk3910@gmail.com>
2024-06-07 14:53:30 +0900

    feat: Solution12928

---
+++ src/Programmers/Solution12928.java
@@ -0,0 +1,13 @@
+package Programmers;
+
+public class Solution12928 {
+  public int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n ; i++) {
+      if (n % i == 0) {
+        answer += i;
+      }
+    }
+    return answer;
+  }
+}
