e1c06b84054614d958de72fd3684e3d372b289c3
soeunc <fkckxk3910@gmail.com>
2024-06-13 12:29:12 +0900

    feat: Solution12954

---
+++ src/Programmers/Solution12954.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class Solution12954 {
+  public long[] solution(int x, int n) {
+    long[] answer = new long[n];
+    for (int i = 1; i <= n; i++) {
+      answer[i - 1] = (long) x * i;
+    }
+    return answer;
+  }
+}
