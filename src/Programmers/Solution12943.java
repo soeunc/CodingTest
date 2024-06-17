e7050cc9f3b030a365f04536efc2298012e0c040
soeunc <fkckxk3910@gmail.com>
2024-06-17 14:18:51 +0900

    feat: Solution12943

---
+++ src/Programmers/Solution12943.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class Solution12943 {
+  public int solution(long num) {
+    int count = 0;
+
+    while (num != 1) {
+      if (num % 2 == 0) {
+        num /= 2;
+      } else  {
+        num = num * 3 + 1;
+      }
+      count++;
+      if (count == 500) {
+        return -1;
+      }
+    }
+    return count;
+  }
+}
