eaf757deccee9637eaab36b445a98a7e20baca6d
soeunc <fkckxk3910@gmail.com>
2024-06-19 15:43:52 +0900

    feat: Solution12948

---
+++ src/Programmers/Solution12948.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class Solution12948 {
+  public String solution(String phone_number) {
+    int length = phone_number.length();
+    
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < length - 4; i++) {
+      sb.append('*');
+    }
+    sb.append(phone_number.substring(length - 4));
+
+    return sb.toString();
+  }
+}
