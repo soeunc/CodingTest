354c909d33231a86be76905dc0ff450eb55d5754
soeunc <fkckxk3910@gmail.com>
2024-07-02 17:25:42 +0900

    feat: Solution12940

---
+++ src/Programmers/Solution12940.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+public class Solution12940 {
+  public int[] solution(int n, int m) {
+    int gcd = gcd(n, m);
+    int lcm = (n * m) / gcd;
+
+    return new int[]{gcd, lcm};
+  }
+
+  private int gcd(int a, int b) {
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+}
